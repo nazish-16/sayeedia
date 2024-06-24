fc3f4d378371eef2cc8225fbba84548d5c746519
Frezz1ck <84974655+nazish-16@users.noreply.github.com>
2024-06-24 13:41:20 +0530

    changed

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
-import React from "react";
-import Navbar from "@/Components/Navbar";
 import Hero from "@/Components/Hero";
+import Navbar from "@/Components/Navbar";
 import Sections from "@/Components/Sections";
-import './globals.css'
 
 export default function Home() {
   return (
