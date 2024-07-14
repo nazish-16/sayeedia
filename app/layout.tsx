b460c64ec6dd6ab4cd69102ea31580efc4abf345
Frezz1ck <84974655+nazish-16@users.noreply.github.com>
2024-07-15 00:21:53 +0530

    scroll behaviour

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className='scroll-smooth' style={{ scrollBehavior: 'smooth' }}>
       <body className={montserrat.className}>{children}</body>
     </html>
   );
